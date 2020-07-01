7df4c61bde19033c472e1b91f9f32d9a25bb5599
Vladimir Tsimbol <yuashick@gmail.com>
2020-07-01 23:50:33 +0500

    add unittest project

---
+++ UnitTestSharpDump/ProgramUnitTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+
+namespace UnitTestSharpDump
+{
+    [TestFixture]
+    public class ProgramUnitTest
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.True(false);
+        }
+    }
+}
---
+++ UnitTestSharpDump/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestSharpDump")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestSharpDump")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e35f362f-de0c-47d6-98d7-5a8ef42f6c83")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
